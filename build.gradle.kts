a1dcf8135101e0b4e590a58f8cbb56df04ca08e3
Matthias Lersch <matthias.lersch@gmail.com>
2026-02-21 16:01:54 +0100

    Version update and vulnerability fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.5"
+	kotlin("jvm") version "2.3.10"
+	kotlin("plugin.spring") version "2.3.10"
+	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -18,21 +18,29 @@ repositories {
 	mavenCentral()
 }
 
-
 dependencies {
+	// Fix CVE-2025-22233 by forcing Spring Framework upgrade
+	implementation(platform("org.springframework:spring-framework-bom:6.2.7"))
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.security:spring-security-crypto:6.4.5")
+	implementation("org.springframework.security:spring-security-crypto")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
+	// Fix CVE-2025-48924 by forcing Commons Lang3 upgrade
+	implementation("org.apache.commons:commons-lang3:3.18.0")
+
+	// Fix CVE-2025-46701, CVE-2025-48976, CVE-2025-49124, CVE-2025-49125, CVE-2025-24813, CVE-2025-55752 by forcing Tomcat upgrade
+	implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.18")
+
 	// Swagger/OpenAPI Documentation
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-	implementation("org.springframework.boot:spring-boot-starter-actuator:6.2.10")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
@@ -42,9 +50,9 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
-	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 }
 
 kotlin {
